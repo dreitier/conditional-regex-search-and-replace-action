440f9c6e57377114363fe4263fd09975c9e89fdd
Christopher Klein <ckl@dreitier.com>
2022-11-16 13:23:54 +0100

    fix: added test for creating a Git context

--- tests/Feature/GitContextTest.php
+++ tests/Feature/GitContextTest.php
@@ -1,102 +1,21 @@
 <?php
-use App\ContentUpdater;
-use App\Variable\Variable;
-use App\Replacer\Replacer;
-use App\Variable\Collection as VariableCollection;
-use App\Replacer\Collection as ReplacerCollection;
+
+use App\Filesystem\Context as FilesystemContext;
 use Illuminate\Support\Facades\Event;
 
-it('can replace a single variable', function () {
-	$variable = new Variable('docker_image_tag', '1.0.0');
-	
-	$variables = new VariableCollection();
-	$variables->add($variable);
-	$regexCollection = (new ReplacerCollection())
-		->add(new Replacer('some_name', 'imageTag: \"(?<docker_image_tag>.*)\"'));		
-	
-	$sut = new ContentUpdater($variable, $regexCollection->items(), $variables);
-	
-	$content = <<<CONTENT
-first: value
-second: value2
-  imageTag: "0.0.666"
-fourth: value4
-CONTENT;
-	
-	$r = $sut->update($content);
-	
-	expect($r)->toBe(<<<CONTENT
-first: value
-second: value2
-  imageTag: "1.0.0"
-fourth: value4
-CONTENT
-);
-});
+it('can open a Git repository', function () {
+    $fs = FilesystemContext::of(__DIR__ . '/../..');
+    $gitOptions = new \App\Git\Options($fs);
+    $ctx = $gitOptions->createContext();
 
-it('can replace the same variable multiple times', function () {
-	$variable = new Variable('docker_image_tag', '1.0.0');
-	
-	$variables = new VariableCollection();
-	$variables->add($variable);
-	$regexCollection = (new ReplacerCollection())
-		->add(new Replacer('some_name', 'imageTag: \"(?<docker_image_tag>.*)\"'));		
-		
-		
-	$sut = new ContentUpdater($variable, $regexCollection->items(), $variables);
-	
-	$content = <<<CONTENT
-first: value
-second: value2
-  imageTag: "0.0.666"
-fourth: value4
-  imageTag: "0.0.666"
-CONTENT;
-	
-	$r = $sut->update($content);
-	
-	expect($r)->toBe(<<<CONTENT
-first: value
-second: value2
-  imageTag: "1.0.0"
-fourth: value4
-  imageTag: "1.0.0"
-CONTENT
-);
-});
+    try {
+        $repo = $ctx->open();
+        $sut = $repo->getLastCommit();
 
-it('can reference another variable', function () {
-	$variable = new Variable('docker_image_tag', '1.0.0');
-	$otherVar = new Variable('custom_var', '4.0.0');
-	
-	$variables = new VariableCollection();
-	$variables
-		->add($variable)
-		->add($otherVar);
-	$regexCollection = (new ReplacerCollection())
-		->add(new Replacer('some_name', 'imageTag: \"(?<docker_image_tag>.*)\" (?<custom_var>\w+)'));		
-		
-	$sut = new ContentUpdater($variable, $regexCollection->items(), $variables);
-	
-	$content = <<<CONTENT
-first: value
-second: value2
-  imageTag: "0.0.666" old_custom_var
-fourth: value4
-  # the next line gets not replaced as the regex does not match the line
-  imageTag: "0.0.666"
-CONTENT;
-	
-	$r = $sut->update($content);
-	
-	expect($r)->toBe(<<<CONTENT
-first: value
-second: value2
-  imageTag: "1.0.0" 4.0.0
-fourth: value4
-  # the next line gets not replaced as the regex does not match the line
-  imageTag: "0.0.666"
-CONTENT
-);
-
+        expect($sut->getId()->toString())->toBeString();
+    } catch (Exception $e) {
+        expect($e->getMessage())->toBeEmpty();
+    } finally {
+        $ctx->close();
+    }
 });
