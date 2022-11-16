e4a8afb1c2e396b0831cc9b4b71a88fb57d5a1c5
Christopher Klein <ckl@dreitier.com>
2022-11-16 13:25:25 +0100

    fix: removed popd/pushd as it is present in the Git abstraction

--- app/Filesystem/Context.php
+++ app/Filesystem/Context.php
@@ -1,40 +1,46 @@
 <?php
+
 namespace App\Filesystem;
+
 use App\Events\LogEvent;
 
 class Context
 {
-	public function __construct(
-		public readonly string $baseDirectory,
-		public readonly string $suffixOfUpdatedFile = '',
-	)
-	{
-		if (!is_dir($baseDirectory)) {
-			LogEvent::fatal("Directory '$baseDirectory' does not exist");
-		}
-	}
-	
-	private ?string $cwd = null;
-	
-	public function pushdBaseDirectory() : string
-	{
-		throw_if(!empty($this->cwd), "pushd before popd");
+    public function __construct(
+        public readonly string $baseDirectory,
+        public readonly string $suffixOfUpdatedFile = '',
+    )
+    {
+        if (!is_dir($baseDirectory)) {
+            LogEvent::fatal("Directory '$baseDirectory' does not exist");
+        }
+    }
 
-		$cwd = getcwd();
-		chdir($this->baseDirectory);
-		
-		$this->cwd = $this->baseDirectory;
-		return $this->cwd;
-	}
-	
-	public function popd(): string
-	{
-		throw_if(!$this->cwd, "No pushd before popd");
-		
-		$newDir = $this->cwd;
-		chdir($newDir);
-		$this->cwd = null;
-		
-		return $newDir;
-	}
+    public static function of($baseDirectory)
+    {
+        return new self($baseDirectory);
+    }
+
+    private ?string $cwd = null;
+
+    public function pushdBaseDirectory(): string
+    {
+
+        $cwd = getcwd();
+        chdir($this->baseDirectory);
+
+        $this->cwd = $this->baseDirectory;
+        return $this->cwd;
+    }
+
+    public function popd(): string
+    {
+        throw_if(!$this->cwd, "No pushd before popd");
+
+        $newDir = $this->cwd;
+        chdir($newDir);
+        $this->cwd = null;
+
+        return $newDir;
+    }
 }
